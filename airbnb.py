eef3387d239cc4e1e642968d84fa2cdd6882e132
Nambukeerthi <nambu935@gmail.com>
2024-11-20 20:44:50 +0530

    Update airbnb.py

--- airbnb.py
+++ airbnb.py
@@ -1,31 +1,45 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from PIL import Image
 import pandas as pd
-import streamlit at st
+from sqlalchemy import create_engine
+import plotly.express as px
+import numpy as np
+import requests
 import json
 
-# READ CSV DATA FILE
-df_csv = pd.read_csv("airbnbdata/airbnb.csv")
-df1_copy=pd.DataFrame(df_csv)
+st.set_page_config(
+        
+        page_title="phoenpe analysis ",
+        page_icon="",
+        layout = "wide"
+    )
 
-# IMAGE COLUME CORRECTION
-# Step 1: Replace single quotes with double quotes (only if the entry is a string)
-df1_copy["images"] = df1_copy["images"].apply(lambda x: x.replace("'", '"') if isinstance(x, str) else x)
-# Step 2: Parse the corrected string into a dictionary using json.loads()
-df1_copy["images"] = df1_copy["images"].apply(lambda x: json.loads(x) if isinstance(x, str) else x)
-# Step 3: Access the 'picture_url' key from the parsed dictionary
-df1_copy["images"] = df1_copy["images"].apply(lambda x: x.get("picture_url", "No URL") if isinstance(x, dict) else "No URL")
 
-# REVIEW SCORE COLUME CORRECTION
-# Step 1: Replace single quotes with double quotes (only if the entry is a string)
-df1_copy["review_scores"] = df1_copy["review_scores"].apply(lambda x: x.replace("'", '"') if isinstance(x, str) else x)
-# Step 2: Parse the corrected string into a dictionary using json.loads()
-df1_copy["review_scores"] = df1_copy["review_scores"].apply(lambda x: json.loads(x) if isinstance(x, str) else x)
-# Step 3: Access the 'picture_url' key from the parsed dictionary
-df1_copy["review_scores"] = df1_copy["review_scores"].apply(lambda x: x.get("review_scores_rating", 0) if isinstance(x, dict) else 0)
+st.title("PHONPE DATA VISUALIZATION AND EXPLORATION")
+st.markdown(" ")
+st.markdown(" ")
+st.markdown(" ")
 
-# HANDLING NULL VALUES
-# df1_copy.isnull().sum() to know null values
-# bedrooms, beds, bathrooms, cleaning_fee
-df1_copy["bedrooms"] = df1_copy["bedrooms"].fillna(0)
-df1_copy["beds"] = df1_copy["beds"].fillna(0)
-df1_copy["bathrooms"] = df1_copy["bathrooms"].fillna(0) 
-df1_copy["cleaning_fee"] = df1_copy["cleaning_fee"].fillna(0)
+with st.sidebar:
+    
+    select = option_menu("Main menu",["HOME","DATA EXPLORATION"])
+    
+    
+if select == "HOME":
+         
+st.markdown(" ")
+        st.markdown(" ")
+        st.markdown(" ")
+        img1 = Image.open("images/phonepe3.jpg")
+        st.image( img1,use_column_width=True,channels="RGB" )
+        st.markdown(" ")
+        st.markdown(" ")
+        st.markdown(" ")
+
+elif select == "DATA EXPLORATION":
+
+        # READ CSV DATA FILE
+        df_csv = pd.read_csv("airbnbdata/airbnb.csv")
+        df1_copy=pd.DataFrame(df_csv)
+
