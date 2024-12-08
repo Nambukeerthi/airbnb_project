0046fe723111b8d20dc8931613c23dda7124e07a
Nambukeerthi <nambu935@gmail.com>
2024-12-08 21:55:08 +0530

    Update airbnb.py

--- airbnb.py
+++ airbnb.py
@@ -88,22 +88,18 @@ def func_all ():
     st.bar_chart(df_room_types.set_index('room_type'))
         
     #average price of neighbourhood
+    st.subheader("Average Price of Host Neighbourhood")    
     avg_neighbour = df.groupby("host_neighbourhood")[["price"]].mean() 
     avg_neighbour.reset_index(inplace=True)
     st.dataframe(avg_neighbour.head(10), use_container_width=True) 
+    st.markdown(" ")
+    st.markdown(" ")
+    st.markdown(" ")     
     st.bar_chart(avg_neighbour.set_index('host_neighbourhood'))
     # styled_df = avg_neighbour_room_new.style.background_gradient(subset=['price'], cmap='coolwarm')
     
-    #total room type price
-    tot_neighbour_room = df.groupby(["host_neighbourhood","room_type"])["price"].sum() 
-    tot_neighbour_room = tot_neighbour_room.reset_index()
-    cols = tot_neighbour_room.columns.tolist()
-    cols[0], cols[1] = cols[1], cols[0]  # Swap 'host_neighbourhood' and 'room_type'
-    tot_neighbour_room = tot_neighbour_room[cols]
-    tot_neighbour_room_new = tot_neighbour_room.pivot(index='host_neighbourhood', columns='room_type', values='price')
-    st.dataframe(tot_neighbour_room_new, use_container_width=True)
-    
     #top host by total reviews
+    st.subheader("Top 10 Reviews")    
     review_group = df[['name','country','review_scores','number_of_reviews']]
     review_group = review_group.reset_index(drop=True)
     top_hotel_reviews = review_group.sort_values(by=['review_scores', 'number_of_reviews'], ascending=[False, False])
@@ -114,14 +110,27 @@ def func_all ():
     'number_of_reviews': 'Number of Reviews',
     'country':'Country Name'})
     st.dataframe(top_hotel_reviews.head(10), use_container_width=True)
+    st.markdown(" ")
+    st.markdown(" ")
+    st.markdown(" ")     
     st.bar_chart(data=top_hotel_reviews.head(10),x='Hotel Name', y='Number of Reviews',horizontal=True,use_container_width=True)
-    
+
+    st.markdown(" ")
+    st.markdown(" ")
+    st.markdown(" ")      
+    #cancelation policy 
+    st.subheader("Cancelation Policy")     
     cancel_policy = df['cancellation_policy'].value_counts().reset_index()
     cancel_policy.columns = ['cancellation_policy', 'count']
     st.dataframe(cancel_policy, use_container_width=True)
     fig1 = px.pie(cancel_policy, values='count', names='cancellation_policy', title='Cancellation Policy')
     st.plotly_chart(fig1)
-    
+
+    st.markdown(" ")
+    st.markdown(" ")
+    st.markdown(" ")     
+    #bedrooms 
+    st.subheader("Bedrooms Types")     
     bedrooms = df['bedrooms'].value_counts().reset_index()
     bedrooms.columns = ['bedrooms', 'count']
     st.dataframe(bedrooms, use_container_width=True)
